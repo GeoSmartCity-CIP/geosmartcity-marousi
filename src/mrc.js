b288045b47fdb521ac566b5472038762c0d646a1
Christos <xr_xarmatzis@outlook.com>
2016-10-26 08:24:26 +0300

    developing

--- src/mrc.js
+++ src/mrc.js
@@ -2,8 +2,9 @@
 
 var options = {
     epsg: "EPSG:4326",
-    bounds: [23.7767658233643, 38.0244979858398, 23.8312721252441, 38.0672760009766],
-    units: 'degrees'
+    bounds: [23.7767658233643, 38.0244979858398, 23.779, 38.04],
+    units: 'degrees',
+    zoom:10
 };
 
 // map initializing
@@ -63,11 +64,10 @@ var osm =  new ol.layer.Tile({
 gsc.map.addLayer(osm);
 
 
-gsc.map.addLayer(layerWFS);
 
 
-gsc.editFeatures.create(container, content, closer, '#mdl-button', gsc.map, url);
-gsc.editFeatures.addLayer(layerWFS);
+
+
 
 
 
@@ -87,6 +87,11 @@ function setInfo() {
 
 gsc.map.addLayer(marousi);
 
+//gsc.map.addLayer(layerWFS);
+
+//gsc.editFeatures.create(container, content, closer, '#mdl-button', gsc.map.olMap, url);
+//gsc.editFeatures.addLayer(layerWFS);
+
 gsc.map.addMousePositionControl('coordinate');
 
 gsc.map.addScaleBarControl('scalebar');
@@ -102,7 +107,7 @@ $('#resetFilterButton').on('click', function () {
     gsc.map.resetFilter('cql', '')
 });
 
-gsc.map.addInfoOnFeatureEvent('nodelist', 50, marousi);
+//gsc.map.addInfoOnFeatureEvent('nodelist', 50, marousi);
 
 
 
@@ -160,3 +165,97 @@ var comment=gsc.GeoComment({
 /*gsc.map.addInfoOnFeatureEvent('nodelist', 50, navarre_cadaster,function(capas){
 	comment.show();
 });*/
+
+ $(document).ready(function(){
+var val = "";
+     $("#submit").click(function(event){
+	 
+         event.preventDefault();
+         var address = $('#address').val();
+
+         $.ajax({
+             type: "GET",
+             data: {address:address},
+             dataType:"json",
+             url:  "http://hub.geosmartcity.eu/MarousiGeocoderServer/geo/RestService/getaddress",
+             //url: "http://localhost:8080/MarousiGeocoderServer/geo/RestService/getaddress",
+             success: function(results) {
+                 //console.log("response:" + results);
+                 $("#data").html('');
+                 var items = [];
+                var pointFeatures =[];
+                 
+                 $.each( results, function( object, value ) {
+                	 
+                	 var point;
+                	 var name;
+                	 var coor;
+                	 var lon;
+                	 var lat;
+                	 $.each(value, function(key, val)
+                       {  
+                		if(key=='geometry')
+                			 {
+                			 	var json = $.parseJSON(val); 
+                			 	lon = json.coordinates[0];
+           			         	lat = json.coordinates[1];
+                			 	items.push([lon,lat]);
+                			 }
+                		if(key=='address')
+                			{
+                				name = val;
+                			}
+                       });
+                	pointFeatures.push([[lon,lat], name]);
+                 });
+                 var iconFeatures=[];
+                 for(var i=0; i<pointFeatures.length; i++)
+				 {
+					var iconFeature = new ol.Feature({
+					   geometry: new ol.geom.Point(ol.proj.transform(pointFeatures[i][0], 'EPSG:4326',     
+					   'EPSG:3857')),
+					   address: pointFeatures[i][1]
+					 });
+					 iconFeatures.push(iconFeature);
+				 }
+				 
+				 
+				 
+				 var vectorSource = new ol.source.Vector({
+				   features: iconFeatures //add an array of features
+				 });
+
+				 var iconStyle = new ol.style.Style({
+				   image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+					 anchor: [0.5, 46],
+					 anchorXUnits: 'fraction',
+					 anchorYUnits: 'pixels',
+					 opacity: 0.75,
+					 src: 'http://findicons.com/files/icons/1030/windows_7/32/pin.png'
+				   }))
+				 });
+
+
+				 var vectorLayer = new ol.layer.Vector({
+				   source: vectorSource,
+				   style: iconStyle
+				 });
+                 
+                 gsc.map.addLayer(vectorLayer);
+                
+                 var lon = items[0];
+                 var lat = items[1];
+                 var test = ol.proj.transform([items[0][0], items[0][1]],'EPSG:4326', 'EPSG:3857');
+                
+                 
+                 
+                 
+                 
+                 
+             },
+             error: function(jqXHR, textStatus, errorThrown) {
+                 alert(' Error in processing! '+textStatus);
+             }
+         });
+     });
+ });
